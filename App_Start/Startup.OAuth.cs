bbc322d0ee862bdacfe7f1c7c1f8da749c2a7c15
Jon Preece <jon@developerhandbook.com>
2016-03-08 14:32:35 +0000

    Added OAuth Part 1

---
+++ App_Start/Startup.OAuth.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using BooksAPI.Core;
+using BooksAPI.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace BooksAPI
+{
+    public partial class Startup
+    {
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var issuer = ConfigurationManager.AppSettings["issuer"];
+            var audience = ConfigurationManager.AppSettings["audience"];
+            var secret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["secret"]);
+
+            app.CreatePerOwinContext(() => new BooksContext());
+            app.CreatePerOwinContext(() => new UserManager<IdentityUser>(new UserStore<IdentityUser>()));
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AllowedAudiences = new[] {audience},
+                IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                {
+                    new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                }
+            });
+            
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat(issuer)
+            });
+        }
+    }
+}
---
+++ Identity/CustomOAuthProvider.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using BooksAPI.Core;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+
+namespace BooksAPI.Identity
+{
+    public class CustomOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            IdentityUser user;
+            if (!AuthenticateUser(context, out user))
+            {
+                return Task.FromResult<object>(null);
+            }
+
+            var props = new AuthenticationProperties(new Dictionary<string, string>
+            {
+                {"audience", context.ClientId ?? string.Empty}
+            });
+
+            var ticket = new AuthenticationTicket(SetClaimsIdentity(context, user), props);
+            context.Validated(ticket);
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId;
+            string clientSecret;
+
+            if (!context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                context.TryGetFormCredentials(out clientId, out clientSecret);
+            }
+
+            if (context.ClientId == null)
+            {
+                context.SetError("invalid_clientId", "client_Id is not set");
+                return Task.FromResult<object>(null);
+            }
+
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+
+        private static ClaimsIdentity SetClaimsIdentity(OAuthGrantResourceOwnerCredentialsContext context, IdentityUser user)
+        {
+            var identity = new ClaimsIdentity("JWT");
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+            identity.AddClaim(new Claim("sub", context.UserName));
+            
+            var userRoles = context.OwinContext.Get<UserManager<IdentityUser>>().GetRoles(user.Id);
+            foreach (var role in userRoles)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, role));
+            }
+
+            SetPrincipal(new ClaimsPrincipal(identity));
+            return identity;
+        }
+
+        private static bool AuthenticateUser(OAuthGrantResourceOwnerCredentialsContext context, out IdentityUser user)
+        {
+            user = null;
+
+            if (string.IsNullOrEmpty(context.UserName) || string.IsNullOrEmpty(context.Password))
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect");
+                return false;
+            }
+            user = context.OwinContext.Get<BooksContext>().Users.FirstOrDefault(u => u.UserName == context.UserName); 
+
+            if (!context.OwinContext.Get<UserManager<IdentityUser>>().CheckPassword(user, context.Password))
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void SetPrincipal(IPrincipal principal)
+        {
+            Thread.CurrentPrincipal = principal;
+            if (HttpContext.Current != null)
+            {
+                HttpContext.Current.User = principal;
+            }
+        }
+    }
+}
